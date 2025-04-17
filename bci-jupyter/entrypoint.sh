350f98da830aec74d698ff694cc6eac227c60854
Victor Gregorio <victor.gregorio@suse.com>
2025-04-17 15:36:26 -0400

    update comments

--- bci-jupyter/entrypoint.sh
+++ bci-jupyter/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# default to uid 1000
+# default to uid 1000 inside the container
 CONT_UID=${APP_UID:-1000}
 CONT_UNAME=jupyter
 
@@ -11,15 +11,14 @@ echo "Starting with $CONT_UNAME($CONT_UID)"
 groupadd mail
 useradd -s /bin/bash -u $CONT_UID -d $CONT_HOME -m $CONT_UNAME
 
-# useradd will not apply skel files if the container homedir is created
-# via a container engine's --mount flag, so force the copy of these files.
+# useradd will not apply skel files if the container homedir is present, so we force it
 cp -ar /etc/skel/. ${CONT_HOME}/
 
 # set ownership of homedir and venv to container user
 chown -R $CONT_UID $CONT_HOME
 chown -R $CONT_UID $VIRTENV
 
-# place container engine CMD into a script to facilitate an exec
+# Containerfile CMD inside a script to facilitate an exec
 echo "$@" > /app/cmd.sh
 chmod +x /app/cmd.sh
 
